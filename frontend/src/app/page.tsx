b373de04f74573909c304a3368b673c474ac1a0b
MATSUO-JUNTA <junta0527@icloud.com>
2024-10-13 23:46:24 +0900

    next.jsとrails連携

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,20 @@
-const Home = () => {
+'use client'
+
+import type { NextPage } from 'next'
+import useSWR from 'swr'
+import { fetcher } from '@/utils/fetcher'
+
+const Home: NextPage = () => {
+  const url = process.env.NEXT_PUBLIC_API_URL + '/api_check'
+  const { data, error } = useSWR(url, fetcher)
+
+  if (error) return <div>An error has occurred.</div>
+  if (!data) return <div>Loading...</div>
+
   return (
-    <div>
-      <p>aaa</p>
-    </div>
+    <>
+      <div>{data.message}</div>
+    </>
   )
 }
 
