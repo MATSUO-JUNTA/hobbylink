7c2cea119936909322882c37aac34062e7ad6be2
MATSUO-JUNTA <junta0527@icloud.com>
2024-10-18 23:46:16 +0900

    Issue #6: error画面共通化

--- frontend/src/app/error.tsx
+++ frontend/src/app/error.tsx
@@ -1,29 +1,20 @@
 'use client'
 
-import { Typography, Box } from '@mui/material'
+import ErrorMessage from './components/ErrorMessage'
 
 const Error = () => {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        textAlign: 'center',
-        height: 'calc(100vh - 64px)',
-        maxWidth: '70%',
-        margin: '0 auto',
-      }}
-    >
-      <Typography variant="h4" sx={{ color: '#1E90FF', mb: 1 }}>
-        500
-      </Typography>
-      <Typography variant="body2">
-        何らかの理由でサーバーが応答していません。
-        <br />
-        時間をおいて再度アクセスするか、 サイトの管理者にお問い合わせください。
-      </Typography>
-    </Box>
+    <ErrorMessage
+      errorCode="500"
+      errorMessage={
+        <>
+          何らかの理由でサーバーが応答していません。
+          <br />
+          時間をおいて再度アクセスするか、
+          サイトの管理者にお問い合わせください。
+        </>
+      }
+    />
   )
 }
 
--- frontend/src/app/not-found.tsx
+++ frontend/src/app/not-found.tsx
@@ -1,27 +1,17 @@
-import { Box, Typography } from '@mui/material'
+import ErrorMessage from './components/ErrorMessage'
 
 const NotFound = () => {
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        textAlign: 'center',
-        height: 'calc(100vh - 64px)',
-        maxWidth: '70%',
-        margin: '0 auto',
-      }}
-    >
-      <Typography variant="h4" sx={{ color: '#1E90FF', mb: 1 }}>
-        404
-      </Typography>
-      <Typography variant="body2">
-        お探しのページは存在しません。
-        <br />
-        一時的にアクセスできない状況にあるか、移動もしくは削除された可能性があります。
-      </Typography>
-    </Box>
+    <ErrorMessage
+      errorCode="404"
+      errorMessage={
+        <>
+          お探しのページは存在しません。
+          <br />
+          一時的にアクセスできない状況にあるか、移動もしくは削除された可能性があります。
+        </>
+      }
+    />
   )
 }
 
