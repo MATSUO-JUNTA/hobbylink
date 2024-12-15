158c9c3d225bb016707a59a9d6e64bdc8885bc4b
MATSUO-JUNTA <junta0527@icloud.com>
2024-12-15 23:35:15 +0900

    カテゴリーソート設定

--- backend/app/controllers/api/v1/categories_controller.rb
+++ backend/app/controllers/api/v1/categories_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::CategoriesController < ApplicationController
   def index
-    categories = Category.all
+    categories = Category.order(:id)
     serializer = params[:include_image] == 'true' ? CategorySerializer : PostCategorySerializer
     render json: categories, each_serializer: serializer, status: :ok
   end
