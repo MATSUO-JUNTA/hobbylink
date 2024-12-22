e7d99de4222e9d23ac1778a7e239e6fe2c42f533
MATSUO-JUNTA <junta0527@icloud.com>
2024-12-23 01:47:43 +0900

    Issue #44: フォロー中、フォロワー一覧画面の追加

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -27,6 +27,8 @@ class User < ApplicationRecord
                                    foreign_key: 'followed_id',
                                    dependent: :destroy,
                                    inverse_of: :followed
+  has_many :followings, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   has_many :notifications, dependent: :destroy
 
   validates :name,      presence: true
